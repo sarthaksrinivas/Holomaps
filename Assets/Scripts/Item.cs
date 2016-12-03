f7677bc4594b28b41ade49e3b86075793523e7c0
AnonymousRandomPerson <chenghanngan.us@gmail.com>
2016-12-02 22:53:10 -0500

    Added pathfinding and path rendering

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// An item that the user is looking for.
+/// </summary>
+public class Item : MonoBehaviour {
+
+    /// <summary> The human-readable name of the item. </summary>
+    public string itemName;
+
+    /// <summary> Whether the item has already been collected by the user. </summary>
+    public bool collected;
+
+    /// <summary> The ID of the item. </summary>
+    public long skuID;
+}
